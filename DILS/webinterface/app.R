74250e4e89743e3f52fa554870b22f70c4f5cb65
clementmaz <clement.mazoyer@seed.univ-lille.fr>
2020-02-12 15:12:48 +0100

    dils

--- DILS/webinterface/app.R
+++ DILS/webinterface/app.R
@@ -1262,12 +1262,10 @@ server <- function(input, output, session = session) {
 	## snakemake command
 	DILS_command <- reactiveVal(0)
 	#observeEvent( input$runABC, {DILS_command(paste('snakemake -p -j 999 --snakefile ../2pops/Snakefile --configfile config_', time_stamp(), '.yaml --cluster-config ../cluster.json --cluster "sbatch --nodes={cluster.node} --ntasks={cluster.n} --cpus-per-task={cluster.cpusPerTask} --time={cluster.time}"', sep=''))})
-#	observeEvent( input$runABC, {DILS_command(paste('DILS_', input$nspecies, 'pop.sh ', time_stamp(), '.yaml &', sep=''))})
-#	output$DILS_command <- renderText({DILS_command()})
 
 	observeEvent( input$runABC, {DILS_command(paste('singularity exec popgenomics.sif snakemake --snakefile /mnt/Snakefile_2pop -p -j 50 --configfile ', time_stamp(), '.yaml', sep=''))})
 	output$DILS_command <- renderText({DILS_command()})
-
+	observeEvent( input$runABC, {DILS_command(system(paste('snakemake --snakefile /mnt/DILS/bin/Snakefile_2pop -p -j 50 --configfile ', time_stamp(), '.yaml &', sep='')))})
 
 
 
